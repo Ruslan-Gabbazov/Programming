7b0378e98371231425413922cbe46db303e4d8f3
Ruslan-Gabbazov <ruslan.gabbazov01@gmail.com>
2023-12-17 23:13:57 +0300

    Add figure drawing and figure scale

--- Programming/Shapes/Trajectory.cs
+++ Programming/Shapes/Trajectory.cs
@@ -1,7 +1,25 @@
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace Programming.Shapes
 {
     public class Trajectory
     {
-        
+        private LissajousFigure Figure { get; } = new LissajousFigure();
+        public double Scale { get; set; }
+
+
+        public void Draw(PictureBox paintBox, PaintEventArgs e)
+        {
+            Figure.Center = new Point { X = paintBox.Width / 2, Y = paintBox.Height / 2 };
+            Figure.Radius = paintBox.Width < paintBox.Height
+                ? paintBox.Width / 2
+                : paintBox.Height / 2;
+
+            var points = Figure.GetPoints(Scale);
+
+            e.Graphics.Clear(Color.White);
+            e.Graphics.DrawPolygon(new Pen(Color.Black, 4), points);
+        }
     }
 }
--- Programming/UI/MainForm.cs
+++ Programming/UI/MainForm.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Drawing;
 using System.Windows.Forms;
+using Programming.Shapes;
 
 namespace Programming.UI
 {
     public partial class MainForm : Form
     {
+        private readonly Trajectory _trajectory = new Trajectory();
+
+        private Label AuthorLabel;
+        private Button ColorSwichButton;
+
+        private Label ConfigLabel;
+        private Panel ControlPanel;
+        private Label FigureBreathingSpeedLabel;
+        private TrackBar FigureBreathSpeed;
+        private Label FigureMoveSpeedLabel;
+        private TrackBar FigureSize;
+        private Label FigureSizeLabel;
+        private Button FigureSwichButton;
+        private System.Windows.Forms.PictureBox PaintBox;
+
+        private TrackBar TrajectorySize;
+        private Label TrajectorySizeLabel;
+        private TrackBar TrajectorySpeed;
+
         public MainForm()
         {
             InitializeComponent();
@@ -47,6 +69,7 @@ namespace Programming.UI
             this.TrajectorySize.Size = new System.Drawing.Size(350, 56);
             this.TrajectorySize.TabIndex = 0;
             this.TrajectorySize.Value = 10;
+            this.TrajectorySize.Scroll += new System.EventHandler(this.TrajectorySize_Scroll);
             // 
             // TrajectorySizeLabel
             // 
@@ -173,7 +196,7 @@ namespace Programming.UI
             this.ConfigLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.ConfigLabel.AutoSize = true;
             this.ConfigLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ConfigLabel.Location = new System.Drawing.Point(130, 21);
+            this.ConfigLabel.Location = new System.Drawing.Point(124, 21);
             this.ConfigLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ConfigLabel.Name = "ConfigLabel";
             this.ConfigLabel.Size = new System.Drawing.Size(144, 20);
@@ -188,19 +211,23 @@ namespace Programming.UI
             this.PaintBox.Margin = new System.Windows.Forms.Padding(4);
             this.PaintBox.Name = "PaintBox";
             this.PaintBox.Size = new System.Drawing.Size(774, 628);
+            this.PaintBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.PaintBox.TabIndex = 3;
             this.PaintBox.TabStop = false;
+            this.PaintBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintBox_Paint);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1182, 628);
             this.Controls.Add(this.PaintBox);
             this.Controls.Add(this.ControlPanel);
             this.MinimumSize = new System.Drawing.Size(1200, 675);
             this.Name = "MainForm";
             this.Text = "Programming";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.TrajectorySize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.TrajectorySpeed)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.FigureSize)).EndInit();
@@ -211,21 +238,25 @@ namespace Programming.UI
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Label AuthorLabel;
+        private void PaintBox_Paint(object sender, PaintEventArgs e)
+        {
+            _trajectory.Scale = TrajectorySize.Value / (double)(TrajectorySize.Maximum - TrajectorySize.Minimum);
+            _trajectory.Draw(PaintBox, e);
+        }
 
-        private System.Windows.Forms.Label ConfigLabel;
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
 
-        private System.Windows.Forms.TrackBar TrajectorySize;
-        private System.Windows.Forms.Label TrajectorySizeLabel;
-        private System.Windows.Forms.TrackBar TrajectorySpeed;
-        private System.Windows.Forms.Label FigureMoveSpeedLabel;
-        private System.Windows.Forms.Label FigureSizeLabel;
-        private System.Windows.Forms.TrackBar FigureSize;
-        private System.Windows.Forms.Label FigureBreathingSpeedLabel;
-        private System.Windows.Forms.TrackBar FigureBreathSpeed;
-        private System.Windows.Forms.Button ColorSwichButton;
-        private System.Windows.Forms.Button FigureSwichButton;
-        private System.Windows.Forms.Panel ControlPanel;
-        private System.Windows.Forms.PictureBox PaintBox;
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            PaintBox.Invalidate();
+        }
+
+        private void TrajectorySize_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
     }
 }
