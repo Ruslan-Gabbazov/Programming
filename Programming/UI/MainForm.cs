9c27805d8c3ff74e469f2a9f7f7c6ca363bf6127
Ruslan-Gabbazov <ruslan.gabbazov01@gmail.com>
2023-12-24 22:52:17 +0300

    Remove unused handlers

--- Programming/UI/MainForm.cs
+++ Programming/UI/MainForm.cs
@@ -10,22 +10,14 @@ namespace Programming.UI
     {
         private readonly Trajectory _trajectory = new Trajectory();
 
-        private Label AuthorLabel;
-        private Button ColorSwichButton;
+        private System.Windows.Forms.Label AuthorLabel;
 
         private Label ConfigLabel;
-        private Panel ControlPanel;
-        private Label FigureBreathingSpeedLabel;
-        private TrackBar FigureBreathSpeed;
-        private Label FigureMoveSpeedLabel;
-        private TrackBar FigureSize;
-        private Label FigureSizeLabel;
-        private Button FigureSwichButton;
+        private System.Windows.Forms.Panel ControlPanel;
         private System.Windows.Forms.PictureBox PaintBox;
 
         private TrackBar TrajectorySize;
         private Label TrajectorySizeLabel;
-        private TrackBar TrajectorySpeed;
 
         public MainForm()
         {
@@ -40,22 +32,11 @@ namespace Programming.UI
         {
             this.TrajectorySize = new System.Windows.Forms.TrackBar();
             this.TrajectorySizeLabel = new System.Windows.Forms.Label();
-            this.TrajectorySpeed = new System.Windows.Forms.TrackBar();
-            this.FigureMoveSpeedLabel = new System.Windows.Forms.Label();
-            this.FigureSizeLabel = new System.Windows.Forms.Label();
-            this.FigureSize = new System.Windows.Forms.TrackBar();
-            this.FigureBreathingSpeedLabel = new System.Windows.Forms.Label();
-            this.FigureBreathSpeed = new System.Windows.Forms.TrackBar();
-            this.ColorSwichButton = new System.Windows.Forms.Button();
-            this.FigureSwichButton = new System.Windows.Forms.Button();
             this.ControlPanel = new System.Windows.Forms.Panel();
             this.AuthorLabel = new System.Windows.Forms.Label();
             this.ConfigLabel = new System.Windows.Forms.Label();
             this.PaintBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.TrajectorySize)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TrajectorySpeed)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.FigureSize)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.FigureBreathSpeed)).BeginInit();
             this.ControlPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PaintBox)).BeginInit();
             this.SuspendLayout();
@@ -81,95 +62,10 @@ namespace Programming.UI
             this.TrajectorySizeLabel.TabIndex = 1;
             this.TrajectorySizeLabel.Text = "Размер траектории";
             // 
-            // TrajectorySpeed
-            // 
-            this.TrajectorySpeed.Location = new System.Drawing.Point(25, 200);
-            this.TrajectorySpeed.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.TrajectorySpeed.Maximum = 20;
-            this.TrajectorySpeed.Name = "TrajectorySpeed";
-            this.TrajectorySpeed.Size = new System.Drawing.Size(350, 56);
-            this.TrajectorySpeed.TabIndex = 2;
-            this.TrajectorySpeed.Value = 1;
-            // 
-            // FigureMoveSpeedLabel
-            // 
-            this.FigureMoveSpeedLabel.AutoSize = true;
-            this.FigureMoveSpeedLabel.Location = new System.Drawing.Point(40, 160);
-            this.FigureMoveSpeedLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.FigureMoveSpeedLabel.Name = "FigureMoveSpeedLabel";
-            this.FigureMoveSpeedLabel.Size = new System.Drawing.Size(319, 17);
-            this.FigureMoveSpeedLabel.TabIndex = 3;
-            this.FigureMoveSpeedLabel.Text = "Скорость перемещения фигуры по траектории";
-            // 
-            // FigureSizeLabel
-            // 
-            this.FigureSizeLabel.AutoSize = true;
-            this.FigureSizeLabel.Location = new System.Drawing.Point(143, 258);
-            this.FigureSizeLabel.Name = "FigureSizeLabel";
-            this.FigureSizeLabel.Size = new System.Drawing.Size(110, 17);
-            this.FigureSizeLabel.TabIndex = 4;
-            this.FigureSizeLabel.Text = "Размер фигуры";
-            // 
-            // FigureSize
-            // 
-            this.FigureSize.Location = new System.Drawing.Point(25, 300);
-            this.FigureSize.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.FigureSize.Name = "FigureSize";
-            this.FigureSize.Size = new System.Drawing.Size(350, 56);
-            this.FigureSize.TabIndex = 5;
-            this.FigureSize.Value = 5;
-            // 
-            // FigureBreathingSpeedLabel
-            // 
-            this.FigureBreathingSpeedLabel.AutoSize = true;
-            this.FigureBreathingSpeedLabel.Location = new System.Drawing.Point(110, 358);
-            this.FigureBreathingSpeedLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.FigureBreathingSpeedLabel.Name = "FigureBreathingSpeedLabel";
-            this.FigureBreathingSpeedLabel.Size = new System.Drawing.Size(182, 17);
-            this.FigureBreathingSpeedLabel.TabIndex = 6;
-            this.FigureBreathingSpeedLabel.Text = "Скорость дыхания фигуры";
-            // 
-            // FigureBreathSpeed
-            // 
-            this.FigureBreathSpeed.Location = new System.Drawing.Point(25, 400);
-            this.FigureBreathSpeed.Margin = new System.Windows.Forms.Padding(4);
-            this.FigureBreathSpeed.Name = "FigureBreathSpeed";
-            this.FigureBreathSpeed.Size = new System.Drawing.Size(350, 56);
-            this.FigureBreathSpeed.TabIndex = 7;
-            this.FigureBreathSpeed.Value = 1;
-            // 
-            // ColorSwichButton
-            // 
-            this.ColorSwichButton.Location = new System.Drawing.Point(99, 476);
-            this.ColorSwichButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.ColorSwichButton.Name = "ColorSwichButton";
-            this.ColorSwichButton.Size = new System.Drawing.Size(210, 25);
-            this.ColorSwichButton.TabIndex = 8;
-            this.ColorSwichButton.Text = "Изменить цвета";
-            this.ColorSwichButton.UseVisualStyleBackColor = true;
-            // 
-            // FigureSwichButton
-            // 
-            this.FigureSwichButton.Location = new System.Drawing.Point(99, 516);
-            this.FigureSwichButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.FigureSwichButton.Name = "FigureSwichButton";
-            this.FigureSwichButton.Size = new System.Drawing.Size(210, 26);
-            this.FigureSwichButton.TabIndex = 9;
-            this.FigureSwichButton.Text = "Изменить фигуру";
-            this.FigureSwichButton.UseVisualStyleBackColor = true;
-            // 
             // ControlPanel
             // 
             this.ControlPanel.Controls.Add(this.AuthorLabel);
             this.ControlPanel.Controls.Add(this.ConfigLabel);
-            this.ControlPanel.Controls.Add(this.FigureSwichButton);
-            this.ControlPanel.Controls.Add(this.ColorSwichButton);
-            this.ControlPanel.Controls.Add(this.FigureBreathSpeed);
-            this.ControlPanel.Controls.Add(this.FigureBreathingSpeedLabel);
-            this.ControlPanel.Controls.Add(this.FigureSize);
-            this.ControlPanel.Controls.Add(this.FigureSizeLabel);
-            this.ControlPanel.Controls.Add(this.FigureMoveSpeedLabel);
-            this.ControlPanel.Controls.Add(this.TrajectorySpeed);
             this.ControlPanel.Controls.Add(this.TrajectorySizeLabel);
             this.ControlPanel.Controls.Add(this.TrajectorySize);
             this.ControlPanel.Dock = System.Windows.Forms.DockStyle.Right;
@@ -229,9 +125,6 @@ namespace Programming.UI
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
             this.Resize += new System.EventHandler(this.MainForm_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.TrajectorySize)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TrajectorySpeed)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.FigureSize)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.FigureBreathSpeed)).EndInit();
             this.ControlPanel.ResumeLayout(false);
             this.ControlPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PaintBox)).EndInit();
